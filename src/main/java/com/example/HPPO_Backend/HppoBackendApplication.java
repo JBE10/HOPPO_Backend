082e86e4f8b73e0c7cc442d1d8bf6a2010313e19
Juan Bautista Espino <bautiespino@icloud.com>
2025-08-20 18:41:56 -0300

    Sure! Pl

---
+++ src/main/java/com/example/HPPO_Backend/HppoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.HPPO_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HppoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HppoBackendApplication.class, args);
+	}
+
+}
