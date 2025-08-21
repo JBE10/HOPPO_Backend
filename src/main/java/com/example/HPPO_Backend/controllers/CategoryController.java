4aa8af89ef1d219179e4fe5bcbe29cc305bf6d52
Juan Bautista Espino <bautiespino@icloud.com>
2025-08-21 12:11:49 -0300

    Implement category management endpoints in CategoryController and CategoryService

--- src/main/java/com/example/HPPO_Backend/controllers/CategoryController.java
+++ src/main/java/com/example/HPPO_Backend/controllers/CategoryController.java
@@ -1,21 +1,27 @@
 package com.example.HPPO_Backend.controllers;
 
 import com.example.HPPO_Backend.entities.Category;
+import com.example.HPPO_Backend.services.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("categories")
 public class CategoryController {
+
     @GetMapping
     public String getCategories() {
-        return "List of categories";
+        CategoryService categoryService = new CategoryService();
+        return  categoryService.getCategories();
     }
     @GetMapping("{categoryId}")
     public String getCategoryById(@PathVariable String categoryId) {
-        return "Category with ID: " + categoryId;
+        CategoryService categoryService = new CategoryService();
+        return categoryService.getCategoryById(categoryId);
     }
     @PostMapping()
     public String createCategory(@RequestBody String category) {
-        return category;
+        CategoryService categoryService = new CategoryService();
+
+        return categoryService.createCategory(category);
     }
 }
