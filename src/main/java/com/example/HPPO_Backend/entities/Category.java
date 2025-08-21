f0f4024c8247f83ceea8c7b30ac6f12a0f8b00c9
Juan Bautista Espino <bautiespino@icloud.com>
2025-08-21 12:11:55 -0300

    Implement category management endpoints in CategoryController and CategoryService

---
+++ src/main/java/com/example/HPPO_Backend/entities/Category.java
@@ -0,0 +1,5 @@
+package com.example.HPPO_Backend.entities;
+
+public class Category {
+    
+}
---
+++ src/main/java/com/example/HPPO_Backend/repositories/CategoryRepository.java
@@ -0,0 +1,5 @@
+package com.example.HPPO_Backend.repositories;
+
+public class CategoryRepository {
+    
+}
