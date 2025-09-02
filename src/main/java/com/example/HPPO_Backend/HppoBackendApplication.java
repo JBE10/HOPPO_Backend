7c8c14149548bfac62de57b2cbfa3e02a63c9233
Juan Bautista Espino <bautiespino@icloud.com>
2025-09-02 18:52:33 -0300

    feat: update security configuration to enhance authorization rules for product and order management

--- src/main/java/com/example/HPPO_Backend/HppoBackendApplication.java
+++ src/main/java/com/example/HPPO_Backend/HppoBackendApplication.java
@@ -1,7 +1,13 @@
 package com.example.HPPO_Backend;
 
+import com.example.HPPO_Backend.entity.Role;
+import com.example.HPPO_Backend.entity.User;
+import com.example.HPPO_Backend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HppoBackendApplication {
@@ -10,4 +16,23 @@ public class HppoBackendApplication {
 		SpringApplication.run(HppoBackendApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner createInitialUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+
+			if (userRepository.findByEmail("vendedor.prueba@tienda.com").isEmpty()) {
+				User vendedor = User.builder()
+						.name("Vendedor")
+						.lastName("Test")
+						.email("vendedor.prueba@tienda.com")
+						.username("vendedor.prueba@tienda.com")
+						.password(passwordEncoder.encode("vendedor123"))
+						.role(Role.VENDEDOR)
+						.build();
+				userRepository.save(vendedor);
+
+			}
+		};
+	}
 }
